046a2a5b4a153beeed3e7d0af0aae4251dd5275a
Shadia Afroz Arin <117361687+Arinthkoeln@users.noreply.github.com>
2025-01-11 10:06:14 +0000

    firstclass

---
+++ class01.py
@@ -0,0 +1,30 @@
+#name = input("user name :")
+#age = input ("user age :")
+#print (name, age)
+
+
+#user_name = "Ahmed"
+#user_name = "arin"
+#print (user_name)
+
+#hourly_wage = input("Please enter your hourly wage: ")
+"""hours_worked = input("How many hours did you work this week? ")
+
+print("Hourly wage:" + hourly_wage , "Hours_worked:" + hours_worked)
+
+#print("Hours_worked:" + hours_worked)
+
+age = str(28)
+print(type(age))
+
+value = float(18)
+print(value)
+print(type(value))"""
+
+##Formating
+#output ="{} is {} years old".format("Arin", 30)
+output ="{0} is {1} years old. she is a {2}. {0} is a independent {3}." 
+print(output.format("Arin", 30 , "Devops Engineer", "women"))
+
+
+
