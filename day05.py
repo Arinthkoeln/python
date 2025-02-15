b60c173d30dcde08d90afa7a6ea6d2d44b9bd9fb
ArinthKoeln <shadia_afroz.arin@smail.th-koeln.de>
2025-02-15 10:15:07 +0100

    "update the day5.py"

---
+++ day05.py
@@ -0,0 +1,34 @@
+#Day 5: Conditionals and Booleans
+"""print(bool("Hello"))  # True
+print(bool(0))              # False
+print(bool(6))              # True
+
+print(bool("Caterpillar"))  # True
+print(bool("0"))             # False
+
+#print(bool([]))             # False
+print(bool(["arin", "ashif"]))   # True
+
+#print(bool(True))           # True
+#print(bool(False))          # False"""
+
+#Comparison operators:
+"""a = [1, 2, 3]
+b = [1, 2, 3]
+print(id(a))
+print(id(b))
+print(a == b)  # True
+print(a is b)  # False"""
+
+age = int(input("How old are you? "))
+
+if age >= 18:
+      print("Sorry, we can't serve you.")
+else:
+    
+    print("welcome")
+
+
+
+
+
